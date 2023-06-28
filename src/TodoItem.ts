ad5eece1cfea0360d3276f423144eefb6643376c
EvanKim <evan977512@gmail.com>
2023-06-27 22:07:14 -0600

    TodoList js ts difference check

---
+++ src/TodoItem.ts
@@ -0,0 +1,19 @@
+class TodoItem {
+  // public id: number; // private, public, protected
+  // public task: string;
+  // public complete: boolean;
+
+  // 접근지정자(public, private, protected) can be assigned in the constructor ==> regares as a property
+  constructor(public id: number, public task: string, public complete: boolean) {
+    this.id = id;
+    this.task = task;
+    this.complete = complete;
+  }
+
+  // no return value ==> void
+  printDetails(): void {
+    console.log(`${this.id}\t${this.task}\t${this.complete ? "(complete)" : "\t(incomplete))"}`);
+  }
+}
+
+export default TodoItem;
