e668ed3176d35d8998f60461ef34bdf973aceee0
EvanKim <evan977512@gmail.com>
2023-06-29 14:08:43 -0600

    added 'add' and 'show' function

--- src/view/TodoConsole.ts
+++ src/view/TodoConsole.ts
@@ -1,13 +1,17 @@
-import * as inquirer from "inquirer";
+// import * as inquirer from "inquirer";
 import TodoCollection from "../service/TodoCollection";
 import TodoItem from "../model/TodoItem";
+import inquirer from "inquirer";
 import { data } from "../data";
 import { Commands } from "../model/Commands";
 
 class TodoConsole {
   private todoCollection: TodoCollection;
 
+  private showCompleted: boolean;
+
   constructor() {
+    this.showCompleted = true;
     const sampleTodos: TodoItem[] = data.map((item) => new TodoItem(item.id, item.task, item.complete));
 
     this.todoCollection = new TodoCollection("My Todo LIST", sampleTodos);
@@ -17,9 +21,12 @@ class TodoConsole {
     console.log(
       `=======${this.todoCollection.userName}======` + `(${this.todoCollection.getItemCounts().incomplete} items todo)`
     );
-    this.todoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+    this.todoCollection.getTodoItems(this.showCompleted).forEach((item) => item.printDetails());
   }
 
+  /**
+   * promptUser -> 사용자에게 명령을 입력받는 메서드
+   */
   promptUser(): void {
     console.clear();
     this.displayTodoList();
@@ -35,9 +42,34 @@ class TodoConsole {
         choices: Object.values(Commands),
       })
       .then((answers) => {
-        if (answers["command"] !== Commands.Quit) {
-          this.promptUser();
+        switch (answers["command"]) {
+          case Commands.Toggle:
+            this.showCompleted = !this.showCompleted;
+            this.promptUser();
+            break;
+          case Commands.Add:
+            this.promptAdd();
+            break;
         }
+      });
+  }
+
+  /**
+   * promptAdd -> methods that prompts the user to add a new task
+   */
+  promptAdd(): void {
+    console.clear();
+    inquirer
+      .prompt({
+        type: "input",
+        name: "add",
+        message: "Enter task: ",
+      })
+      .then((answers) => {
+        if (answers["add"] !== "") {
+          this.todoCollection.addTodo(answers["add"]);
+        }
+        this.promptUser();
       });
   }
 }
